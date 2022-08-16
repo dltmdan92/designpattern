cf90bebb1ee0dbf875dabe73b20c7a2e68c0481d
dltmdan92 <dltmdan92@naver.com>
2022-08-16 23:49:24 +0900

    ch02 옵저버 패턴 적용하기 전 + 주석 추가

--- src/main/java/me/seungmoo/designpattern/headfirst/ch02/WeatherDataBeforeObserverPattern.java
+++ src/main/java/me/seungmoo/designpattern/headfirst/ch02/WeatherDataBeforeObserverPattern.java
@@ -26,6 +26,7 @@ public class WeatherDataBeforeObserverPattern {
         float pressure = getPressure();
 
         // 이 부분은 코드 변화가 있을 수 있으므로 캡슐화해야한다.
+        // 또한 구체적인 코드 구현에 의존한다. (실제 인스턴스는 몰라도 된다. interface만 갖다 쓰도록 한다.)
         currenConditionsDisplay.update(temperature, humidity, pressure);
         statisticsDisplay.update(temperature, humidity, pressure);
         forecastDisplay.update(temperature, humidity, pressure);
