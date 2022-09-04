df07807e5c1ef2aa7aafa80526f7965c5962c042
dltmdan92 <dltmdan92@naver.com>
2022-09-04 19:07:20 +0900

    ch05 싱글톤 패턴 - 멀티쓰레딩 문제에 취약한 싱글톤

---
+++ src/main/java/me/seungmoo/designpattern/headfirst/ch05/ChocolateBoiler.java
@@ -0,0 +1,49 @@
+package me.seungmoo.designpattern.headfirst.ch05;
+
+public class ChocolateBoiler {
+    private boolean empty;
+    private boolean boiled;
+
+    private static ChocolateBoiler instance;
+
+    private ChocolateBoiler() {
+        this.empty = true;
+        this.boiled = false;
+    }
+
+    public void fill() {
+        if (isEmpty()) {
+            this.empty = false;
+            this.boiled = false;
+        }
+    }
+
+    public static ChocolateBoiler getInstance() {
+        if (ChocolateBoiler.instance == null) {
+            instance = new ChocolateBoiler();
+        }
+        return instance;
+    }
+
+    public void drain() {
+        if (!isEmpty() && isBoiled()) {
+            // 끊인 재료를 다음 단계로 넘김
+            this.empty = true;
+        }
+    }
+
+    public void boil() {
+        if (!isEmpty() && !isBoiled()) {
+            // 재료를 끓임
+            this.boiled = true;
+        }
+    }
+
+    private boolean isEmpty() {
+        return empty;
+    }
+
+    public boolean isBoiled() {
+        return boiled;
+    }
+}
