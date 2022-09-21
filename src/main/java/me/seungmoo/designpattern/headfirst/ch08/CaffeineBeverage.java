5cbc41ffe55c8c7147a6aecde369194fe154c8be
dltmdan92 <dltmdan92@naver.com>
2022-09-21 22:27:41 +0900

    ch08 템플릿 메서드 패턴 - 간단 예제 구현

---
+++ src/main/java/me/seungmoo/designpattern/headfirst/ch08/CaffeineBeverage.java
@@ -0,0 +1,28 @@
+package me.seungmoo.designpattern.headfirst.ch08;
+
+public abstract class CaffeineBeverage {
+
+    /**
+     * 템플릿 메소드
+     * - 어떤 알고리즘의 템플릿(틀) 역할을 함.
+     * - 여기서는 카페인 음료를 만드는 알고리즘의 템플릿
+     */
+    public final void prepareRecipe() {
+        // 템플릿 내에서 알고리즘의 각 단계는 메소드로 표현
+        // 어떤 클래스는 abstract class에서 표현
+        // 또 다른 어떤 클래스는 sub class에서 구현
+        boilWater();
+        brew();
+        pourInCup();
+        addCondiments();
+    }
+
+    protected void boilWater() {
+        System.out.println("물 끓이는 중");
+    }
+    protected abstract void brew();
+    protected void pourInCup() {
+        System.out.println("컵에 따르는 중");
+    }
+    protected abstract void addCondiments();
+}
---
+++ src/main/java/me/seungmoo/designpattern/headfirst/ch08/Tea.java
@@ -0,0 +1,12 @@
+package me.seungmoo.designpattern.headfirst.ch08;
+
+public class Tea extends CaffeineBeverage {
+    @Override
+    public void brew() {
+        System.out.println("찻잎을 우려내는 중");
+    }
+    @Override
+    public void addCondiments() {
+        System.out.println("레몬을 추가하는 중");
+    }
+}
